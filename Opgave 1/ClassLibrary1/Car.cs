047159b7fc8c5f60d2cd73d61f023d7922010ddb
MrMikkel <79083834+MrMikkel@users.noreply.github.com>
2022-02-22 14:00:07 +0100

    Add files via upload

---
+++ Opgave 1/ClassLibrary1/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Car
+    {
+        private int _id;
+        private string _model;
+        private double _price;
+        private string _licensePlate;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value==0)
+                {
+                    throw new ArgumentOutOfRangeException("id kan ikke være 0");
+                }
+                _id = value;
+            }
+        }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value.Length < 4)
+                {
+                    throw new ArgumentOutOfRangeException("Model name is too short");
+                }
+                _model = value;
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentOutOfRangeException("Giv en ordenlig pris");
+                }
+                _price = value;
+            }
+        }
+
+        public string LicensePlate
+        {
+            get => _licensePlate;
+            set
+            {
+                if (value==null)
+                {
+                    throw new ArgumentNullException("værdien kan ikke være null");
+                }
+                if (value.Length<2||value.Length>7)
+                {
+                    throw new ArgumentOutOfRangeException("nummerpladen er for lang eller for kort");
+                }
+                _licensePlate = value;
+            }
+        }
+
+        public Car(int id, string model, double price, string licensePlate)
+        {
+            Id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+
+
+
+
+
+    }
+}
---
+++ Opgave 1/ClassLibraryTests/CarTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            Car testcar = new Car(1, "Hyundai i20", 17500, "MINBIL1");
+            Assert.AreEqual(testcar.Id,1);
+            Assert.AreEqual(testcar.Model, "Hyundai i20");
+            Assert.AreEqual(testcar.Price, 17500);
+            Assert.AreEqual(testcar.LicensePlate, "MINBIL1");
+        }
+        [TestMethod()]
+        public void IdTest()
+        {
+            Car testcar = new Car(1, "Hyundai i20", 17500, "MINBIL1");
+            testcar.Id = 2;
+            Assert.AreEqual(testcar.Id, 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testcar.Id = 0);
+        }
+        [TestMethod()]
+        public void ModelTest()
+        {
+            Car testcar = new Car(1, "Hyundai i20", 17500, "MINBIL1");
+            testcar.Model = "Hyundai i30";
+            Assert.AreEqual(testcar.Model, "Hyundai i30");
+            testcar.Model = "Hyun";
+            Assert.AreEqual(testcar.Model, "Hyun");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testcar.Model = "Hyu");
+        }
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Car testcar = new Car(1, "Hyundai i20", 17500, "MINBIL1");
+            testcar.Price = 176000;
+            Assert.AreEqual(testcar.Price, 176000);
+            testcar.Price = 1;
+            Assert.AreEqual(testcar.Price, 1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testcar.Price = 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testcar.Price = -100);
+        }
+        [TestMethod()]
+        public void LicenseplateTest()
+        {
+            Car testcar = new Car(1, "Hyundai i20", 17500, "MINBIL1");
+            testcar.LicensePlate = "MINBIL2";
+            Assert.AreEqual(testcar.LicensePlate, "MINBIL2");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testcar.LicensePlate = "MINBIL123");
+            testcar.LicensePlate = "MI";
+            Assert.AreEqual(testcar.LicensePlate, "MI");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testcar.LicensePlate = "M");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => testcar.LicensePlate = "");
+            Assert.ThrowsException<ArgumentNullException>(() => testcar.LicensePlate = null);
+
+
+        }
+    }
+}
